d8375fefcbe3ebee83c536cc978d7967f4357793
Prizrack <karniz0949@gmail.com>
2026-08-02 14:25:40 +1000

    Update utils.js

--- Bee-Clicker/js/utils.js
+++ Bee-Clicker/js/utils.js
@@ -1,11 +1,20 @@
 const ClickerUtils = {
     formatNumber(num) {
-        if (num === null || isNaN(num)) return "0";
-        if (num < 1000) return num.toFixed(1).replace(".0", "");
-        const suffixes = ["", "K", "M", "B", "T"];
+        if (num === null || num === undefined || isNaN(num)) return "0";
+        if (num < 0) return "-" + this.formatNumber(Math.abs(num));
+        if (num < 1000) {
+            return Math.floor(num).toString();
+        }
+        
+        const suffixes = ["", "K", "M", "B", "T", "Q"];
         const i = Math.floor(Math.log10(num) / 3);
-        if (i >= suffixes.length) return num.toExponential(2);
-        const formatted = (num / Math.pow(10, i * 3)).toFixed(1);
-        return formatted.replace(".0", "") + suffixes[i];
+        
+        if (i >= suffixes.length) {
+            return num.toExponential(2);
+        }
+        
+        const value = num / Math.pow(10, i * 3);
+        const formatted = value.toFixed(1);
+        return formatted.replace(/\.0$/, "") + suffixes[i];
     }
 };
